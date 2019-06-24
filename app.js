451699ff48bfafea0c79c4af1c0dbe8f935a0f51
Christon <christonja@gmail.com>
2019-06-24 14:59:46 +0800

    update database for mlab

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ express        = require("express"),
 app            = express();
  
 // APP CONFIG
-mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
+
+var url = process.env.DATABASEURL || "mongodb://localhost/restful_blog_app"
+mongoose.connect(url, {useNewUrlParser: true})
+
+// mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
+
 mongoose.set('useFindAndModify', false);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -104,5 +109,6 @@ app.delete("/blogs/:id", function(req, res){
     //redirect somewhere
 });
 
-app.listen(process.env.PORT, process.env.IP);
-// app.listen(3000);
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+    console.log("The Blog Server Has Started");
+});
